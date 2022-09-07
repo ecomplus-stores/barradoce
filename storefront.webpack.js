80b41b86991090e20ea6637392eb86d72982dd72
Matheus Reis <matheusgnreis@gmail.com>
2022-09-07 04:00:17 -0300

    chore(search-engine): insert search without ad relevance

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const dirSearchAlias = path.resolve(__dirname, 'template/js/lib/search-engine')
+const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 
 module.exports = () => ({
   resolve: {
     alias: {
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js')
     }
   }
