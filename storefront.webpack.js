58e86a49aeb9b096685b8c61b6053da94f4eb30c
matheusgnreis <matheus@e-com.club>
2022-09-28 12:22:34 -0300

    chore: update image

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -8,7 +8,9 @@ module.exports = () => ({
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js')
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
+      './js/APicture.js': path.resolve(__dirname, 'template/js/custom-js/js/APicture.js'),
+      './html/APicture.html': path.resolve(__dirname, 'template/js/custom-js/html/APicture.html')
     }
   }
 })
