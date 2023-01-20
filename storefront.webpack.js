eb6d1310f342710bd82aaf3264842c80bf99d7cf
matheusgnreis <matheus@e-com.club>
2023-01-20 10:22:10 -0300

    chore: update store

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,13 @@ const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 module.exports = () => ({
   resolve: {
     alias: {
-      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term')
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
+      './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
+      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
+      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
+      './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html')
     }
   }
 })
--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,19 @@
+// Add your custom JavaScript for storefront pages here.
+storefront.on('widget:@ecomplus/widget-fb-pixel', function () {
+    const $searchInput = document.getElementById('search-input')
+    const $searchButton = document.querySelector('.header__search-btn')
+    let searchInputValue = $searchInput.value
+    $searchInput.addEventListener('keyup', e => {
+        searchInputValue = e.target.value
+        $searchInput.classList.remove('focus-input')
+    })
+    $searchButton.addEventListener('click', e => {
+        if (!searchInputValue) {
+            $searchInput.focus({focusVisible: true})
+            $searchInput.classList.add('focus-input')
+            e.preventDefault()
+        }
+    })
 
+
+})
