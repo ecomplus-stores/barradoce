0c2c785e87fe23365c189a3e5dd8cfe98f1a45a9
Leonardo Matos <leomp120894@gmail.com>
2023-05-09 11:59:34 -0300

    Deploy with min instances 1

--- .functions/index.js
+++ .functions/index.js
@@ -4,4 +4,6 @@ const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'false'
 
-exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
+exports.ssr = functions
+  .runWith({ minInstances: 1 })
+  .https.onRequest((req, res) => ssr(req, res))
