2e06815499ce6e07eee2f2115f6fde4b18415f2a
Leonardo Matos <leomp120894@gmail.com>
2023-05-09 20:06:31 -0300

    Start copying APicture.js to overwrite

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,13 +6,12 @@ module.exports = () => ({
   resolve: {
     alias: {
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
+      './js/APicture.js': path.resolve(__dirname, 'template/js/custom-js/components/APicture.js'),
       './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
-      //'./js/APicture.html': path.resolve(__dirname, 'template/js/custom-js/js/APicture.html'),
-      //'./js/APicture.js': path.resolve(__dirname, 'template/js/custom-js/js/APicture.js'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html')
     }
   }
