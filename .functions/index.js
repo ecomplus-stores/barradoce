89ceac8100071ddd7c88598a0635248d68692aef
Leonardo Matos <leomp120894@gmail.com>
2023-05-20 13:33:54 -0300

    Test intercepting reverse proxy requests (fix proxy req host)

--- .functions/index.js
+++ .functions/index.js
@@ -12,51 +12,60 @@ exports.ssr = functions
   })
   .https.onRequest(async (req, res) => {
     if (req.url.startsWith('/reverse-proxy/')) {
-      const { headers } = req
-      headers['origin'] = headers['x-forwarded-host']
-      delete headers['forwarded']
-      delete headers['via']
-      delete headers['traceparent']
-      delete headers['upgrade-insecure-requests']
-      delete headers['x-timer']
-      delete headers['x-varnish']
-      Object.keys(headers).forEach((headerName) => {
-        if (
-          headerName.startsWith('x-forwarded-')
-          || headerName.startsWith('cdn-')
-          || headerName.startsWith('fastly-')
-          || headerName.startsWith('x-firebase-')
-          || headerName.startsWith('x-cloud-')
-          || headerName.startsWith('x-appengine-')
-          || headerName.startsWith('function-')
-        ) {
-          delete headers[headerName]
-        }
-      })
-      console.log({
-        url: req.query.url,
-        headers
-      })
+      let proxyUrl
       try {
-        const response = await axios.get(req.query.url, {
-          headers,
-          timeout: 3000,
-          responseType: 'text',
-          validateStatus: (status) => {
-            return Boolean(status)
+        proxyUrl = new URL(req.query.url)
+      } catch {
+      }
+      if (proxyUrl) {
+        const { headers } = req
+        headers['origin'] = headers['x-forwarded-host']
+        headers['host'] = proxyUrl.hostname
+        delete headers['forwarded']
+        delete headers['via']
+        delete headers['traceparent']
+        delete headers['upgrade-insecure-requests']
+        delete headers['x-timer']
+        delete headers['x-varnish']
+        Object.keys(headers).forEach((headerName) => {
+          if (
+            headerName.startsWith('x-forwarded-')
+            || headerName.startsWith('cdn-')
+            || headerName.startsWith('fastly-')
+            || headerName.startsWith('x-firebase-')
+            || headerName.startsWith('x-cloud-')
+            || headerName.startsWith('x-appengine-')
+            || headerName.startsWith('function-')
+          ) {
+            delete headers[headerName]
           }
         })
-        res.status(response.status)
-        if (response.headers) {
-          Object.keys(response.headers).forEach((headerName) => {
-            res.set(headerName, response.headers[headerName])
+        console.log({
+          url: proxyUrl.href,
+          headers
+        })
+        try {
+          const response = await axios.get(proxyUrl.href, {
+            headers,
+            timeout: 3000,
+            responseType: 'text',
+            validateStatus: (status) => {
+              return Boolean(status)
+            }
           })
+          res.status(response.status)
+          if (response.headers) {
+            Object.keys(response.headers).forEach((headerName) => {
+              res.set(headerName, response.headers[headerName])
+            })
+          }
+          res.send(response.data)
+        } catch (err) {
+          console.error(err)
+          res.status(400).send(err.message)
         }
-        res.send(response.data)
-      } catch (err) {
-        console.error(err)
-        res.status(400).send(err.message)
       }
+      res.sendStatus(400)
     } else {
       return ssr(req, res)
     }
