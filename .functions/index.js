3c8ffe964980e7d18b0f7d33c65e6f5af6c13e13
Leonardo Matos <leomp120894@gmail.com>
2023-05-22 18:03:39 -0300

    Update to `@ecomplus/storefront-template` v2.0.0-beta.234

--- .functions/index.js
+++ .functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions')
-const axios = require('axios')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
@@ -10,77 +9,4 @@ exports.ssr = functions
     memory: '512MB',
     minInstances: 2,
   })
-  .https.onRequest(async (req, res) => {
-    if (req.url.startsWith('/reverse-proxy/')) {
-      let proxyUrl
-      try {
-        proxyUrl = new URL(req.query.url)
-      } catch {
-      }
-      if (proxyUrl) {
-        const { headers } = req
-        headers['origin'] = headers['x-forwarded-host']
-        headers['host'] = proxyUrl.hostname
-        headers['accept'] = 'text/plain,text/html,application/xhtml+xml,application/javascript,application/x-javascript'
-        headers['accept-encoding'] = 'gzip'
-        delete headers['forwarded']
-        delete headers['via']
-        delete headers['traceparent']
-        delete headers['upgrade-insecure-requests']
-        delete headers['x-timer']
-        delete headers['x-varnish']
-        delete headers['x-orig-accept-language']
-        Object.keys(headers).forEach((headerName) => {
-          if (
-            headerName.startsWith('x-forwarded-')
-            || headerName.startsWith('cdn-')
-            || headerName.startsWith('fastly-')
-            || headerName.startsWith('x-firebase-')
-            || headerName.startsWith('x-cloud-')
-            || headerName.startsWith('x-appengine-')
-            || headerName.startsWith('function-')
-          ) {
-            delete headers[headerName]
-          }
-        })
-        console.log({
-          url: proxyUrl.href,
-          headers
-        })
-        try {
-          const response = await axios.get(proxyUrl.href, {
-            headers,
-            timeout: 3000,
-            responseType: 'text',
-            validateStatus: (status) => {
-              return Boolean(status)
-            }
-          })
-          console.log(response.headers)
-          res.status(response.status)
-          Object.keys(response.headers).forEach((headerName) => {
-            switch (headerName) {
-              case 'transfer-encoding':
-              case 'connection':
-              case 'strict-transport-security':
-              case 'alt-svc':
-              case 'server':
-                break
-              default:
-                res.set(headerName, response.headers[headerName])
-            }
-          })
-          if (!response.headers['access-control-allow-origin']) {
-            res.set('access-control-allow-origin', '*')
-          }
-          return res.send(response.data)
-        } catch (err) {
-          console.error(err)
-          return res.status(400).send(err.message)
-        }
-      }
-      res.sendStatus(400)
-    } else {
-      return ssr(req, res)
-    }
-  })
+  .https.onRequest((req, res) => ssr(req, res))
