f61106840e7b04f1182b451b28cb1c29a6fd2a69
Leonardo Matos <leomp120894@gmail.com>
2023-06-21 14:17:58 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -1,58 +1,14 @@
-const { initializeApp } = require('firebase-admin/app')
-const { getFirestore, Timestamp } = require('firebase-admin/firestore')
 const { onRequest } = require('firebase-functions/v2/https')
-const { warn } = require('firebase-functions/logger')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
-initializeApp()
-
 exports.ssr2 = onRequest({
   concurrency: 80,
   minInstances: 0,
   memory: '2GiB',
-}, async (req, res) => {
-  let cacheRef
-  try {
-    const db = getFirestore()
-    cacheRef = db.doc(`ssrCache/${req.path}`)
-    const cacheDoc = await cacheRef.get()
-    if (cacheDoc.exists) {
-      const { headers, status, body } = doc.data()
-      Object.keys(headers).forEach((headerName) => {
-        res.set(headerName, headers[headerName])
-      })
-      res.status(status || 200).send(body)
-    }
-  } catch (err) {
-    cacheRef = null
-    warn(err)
-  }
-  const _send = res.send
-  res.send = function (body) {
-    body = html
-    if (!res.headersSent) {
-      _send.apply(res, arguments)
-    }
-    if (cacheRef) {
-      cacheRef.set({
-        headers: res.getHeaders(),
-        status: res.statusCode,
-        body,
-        __timestamp: Timestamp.now()
-      }).catch(warn)
-    }
-  }
-  const _end = res.end
-  res.end = function () {
-    if (!res.headersSent) {
-      _end.apply(res, arguments)
-    }
-  }
-  return ssr(req, res)
-})
+}, ssr)
 
 exports.reverseproxy = onRequest({
   concurrency: 80,
