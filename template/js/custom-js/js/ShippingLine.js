791b0da855a57132d790409dd512c64419ec6f47
matheusgnreis <matheus@e-com.club>
2024-03-28 14:06:28 -0300

    chore: update shipping

--- template/js/custom-js/js/ShippingLine.js
+++ template/js/custom-js/js/ShippingLine.js
@@ -47,14 +47,14 @@ import {
         const date = new Date()
         const today = date.getDay()
         const hour = date.getHours()
-        if (((today === 6) || (today === 0) || (today === 5 && hour >= 11)) && this.serviceCode && this.serviceCode.includes('retire')) {
+        if (((today === 6) || (today === 0) || ((today === 5 || today === 4) && hour >= 11)) && this.serviceCode && this.serviceCode.includes('retire')) {
           return 'Na segunda, após as 14h'
         } else if (((today > 0 && today < 6  && hour < 11)) && this.serviceCode && this.serviceCode.includes('retire')) {
           return 'Hoje mesmo, após as 14h'
         } else if (((today > 0 && today < 6  && hour >= 11)) && this.serviceCode && this.serviceCode.includes('retire')) {
           return 'A partir de amanhã, após as 14h'
         }
-        if (((today === 6) || (today === 0) || (today === 5 && hour >= 11)) && this.serviceCode && this.serviceCode.includes('avulso')) {
+        if (((today === 6) || (today === 0) || ((today === 5 || today === 4) && hour >= 11)) && this.serviceCode && this.serviceCode.includes('avulso')) {
           return 'Receba segunda-feira'
         } else if (((today > 0 && today < 6  && hour < 11)) && this.serviceCode && this.serviceCode.includes('avulso')) {
           return 'Receba hoje'
